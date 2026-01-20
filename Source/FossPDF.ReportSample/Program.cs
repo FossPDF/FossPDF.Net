2c6953c8d6d62bea00c3aa6e04295dd969bead81
Adam Williams <psc@adamwilliams.eu>
2026-01-20 23:33:05 +0000

    Flesh out example

--- Source/FossPDF.ReportSample/Program.cs
+++ Source/FossPDF.ReportSample/Program.cs
@@ -1,7 +1,10 @@
+using FossPDF.Drawing;
 using FossPDF.Fluent;
+using FossPDF.FontSubset.Glue;
 using FossPDF.ReportSample;
 using FossPDF.ReportSample.Layouts;
 
 var model = DataSource.GetReport();
 var report = new StandardReport(model);
+FontManager.RegisterSubsetCallback(BuiltInFontSubsetting.Callback);
 report.GeneratePdf("out.pdf");
